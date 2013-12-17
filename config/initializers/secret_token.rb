2db9202a011d2f0c6e508f34daa47b9d9933a6eb
Jim Weirich <jim.weirich@gmail.com>
2013-12-17 13:46:03 -0500

    Creatio ex Nihilo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wyriki::Application.config.secret_key_base = '719e1c84ee0e00e8a73a92424658d0d14147cf70353e4b019b5cfdadda0f72ed2bdfb8bd7b84adf286210c5b2c716b42686e05c1c80f28fac9b5ade178c95088'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wyriki::Application.config.session_store :cookie_store, key: '_wyriki_session'
